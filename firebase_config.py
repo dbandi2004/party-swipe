6ddb2e3abb9e515e773b9c525e16a4991adf31ce
Deena Bandi <dbandi@usc.edu>
2025-07-10 13:38:45 -0700

    Fix: use correct Firebase secret filename

--- firebase_config.py
+++ firebase_config.py
@@ -2,7 +2,8 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Load your service account key
-cred = credentials.Certificate("partymatch-3504a-firebase-adminsdk-fbsvc-8329e80f14.json")
+cred = credentials.Certificate("partymatch-key.json")
+
 firebase_admin.initialize_app(cred)
 
 # Get Firestore DB
